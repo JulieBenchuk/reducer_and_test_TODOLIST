c2273ca8eec3e5bf12b30d99c393d87208bb90c4
JulieBenchuk <juliebenchuk@gmail.com>
2022-05-17 23:49:29 +0300

    add CHANGE-TODOLIST-FILTER reducer/test

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -3,7 +3,7 @@ import {TodolistType} from "../App";
 import {colors} from "@material-ui/core";
 
 
-type allACTypes = removeTodolistACType | addTodolistACType | changeTodolistTitleACType
+type allACTypes = removeTodolistACType | addTodolistACType | changeTodolistTitleACType | changeFilterACType
 export const todolistsReducer = (state: Array<TodolistType>, action: allACTypes) => {
     switch (action.type) {
         case "REMOVE-TODOLIST":
@@ -12,7 +12,9 @@ export const todolistsReducer = (state: Array<TodolistType>, action: allACTypes)
             let newTodolist = {id: action.payload.id, title: action.payload.title, filter: "all"}
             return [...state, newTodolist];
         case "CHANGE-TODOLIST-TITLE":
-            return state.map(t=>t.id===action.payload.id ? {...t, title: action.payload.title} : t)
+            return state.map(t=>t.id===action.payload.id ? {...t, title: action.payload.title} : t);
+        case "CHANGE-TODOLIST-FILTER":
+            return state.map(t=>t.id===action.payload.id ? {...t, filter: action.payload.filter} : t)
         default:
             return state;
     }
@@ -39,6 +41,14 @@ export const changeTodolistTitleAC = (id: string, title: string) => {
     return {
         type: 'CHANGE-TODOLIST-TITLE',
         payload: {id, title}
+    } as const
+}
+
+type changeFilterACType = ReturnType<typeof changeFilterAC>
+export const changeFilterAC = (id: string, filter: string) => {
+    return {
+        type: 'CHANGE-TODOLIST-FILTER',
+        payload: {id, filter}
     } as const
 }
 
